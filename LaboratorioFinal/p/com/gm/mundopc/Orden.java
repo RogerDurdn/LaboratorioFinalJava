bc94443d70ba6783ce301c31215cb1220c8b04bc
ElyPus <ElyPus@ElyPus-PC>
2018-02-04 12:11:28 -0600

    progress

--- LaboratorioFinal/p/com/gm/mundopc/Orden.java
+++ LaboratorioFinal/p/com/gm/mundopc/Orden.java
@@ -5,13 +5,26 @@ public class Orden {
 	private Computadora [] computadoras;
 	private static int conOrdenes;
 	private int conComputadoras;
-	private int maxComputadoras;
+	private int maxComputadoras = 5;
 	
 		private Orden() {
 			this.idOrden = ++conOrdenes;
 		}
-		public Orden() {
+		public void agregarComputadoras(Computadora computadora) {
+			if(conComputadoras<maxComputadoras) {
+				computadoras[conComputadoras++] = computadora;
+			}
+			else {
+				System.out.println("A llegado al maximo de computadoras por pedido.!!");
+			}
 			
+		}
+		public void emostrarOrden() {
+			System.out.println("Orden: #"+idOrden);
+			System.out.println("Computadoras de la orden:");
+			for(int i=0; i < conComputadoras; i++) {
+				System.out.println(computadoras[i]);
+			}
 		}
 		
 }
--- LaboratorioFinal/p/mundopc/MundoPC.java
+++ LaboratorioFinal/p/mundopc/MundoPC.java
@@ -1,5 +1,22 @@
 package mundopc;
-
+import com.gm.mundopc.*;
 public class MundoPC {
+	
+	public static void main(String[] args) {
+		
+		Monitor [] lMonitores;
+		lMonitores = new Monitor[10];
+		
+		lMonitores [0] = new Monitor("sony",15);
+		lMonitores [1] = new Monitor("daewo",12);
+		lMonitores [2] = new Monitor("panasonic",);
+		lMonitores [3] = new Monitor("",);
+		lMonitores [4] = new Monitor("",);
+		lMonitores [5] = new Monitor("",);
+		lMonitores [6] = new Monitor("",);
+		lMonitores [7] = new Monitor("",);
+		lMonitores [8] = new Monitor("",);
+		lMonitores [9] = new Monitor("",);
+	}
 
 }
